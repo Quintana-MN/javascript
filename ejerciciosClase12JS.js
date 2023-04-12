3fe6a951b82c450b95fbd1450554142d0873c62a
Matias Nicolas Quintana <matiasnicolasquintana09@gmail.com>
2023-04-12 19:39:45 -0300

    mejoré el ejercicio

---
+++ ejerciciosClase12JS.js
@@ -0,0 +1,10 @@
+const numbers = [10, 19, 65, 34, 09]
+
+function deleteLastNumberOfArray(numbers){
+    console.log("hola, estoy aprendiendo a usar el pop")
+    console.log(numbers)
+    numbers.pop()
+    console.log(numbers)
+}
+
+deleteLastNumberOfArray(numbers)
