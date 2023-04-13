659400a317ddfb02b3fa7c9b60884961ca779692
Matias Nicolas Quintana <matiasnicolasquintana09@gmail.com>
2023-04-12 23:30:30 -0300

    correguí el .pop por .shift :)

--- ejerciciosClase12JS.js
+++ ejerciciosClase12JS.js
@@ -3,7 +3,7 @@ const numbers = [10, 19, 65, 34, 09]
 function deleteLastNumberOfArray(numbers){
     console.log("hola, estoy aprendiendo a usar el pop")
     console.log(numbers)
-    numbers.pop()
+    numbers.shift()
     console.log(numbers)
 }
 
